815982e59991887bc7bbfd8092f9cd74895b69fe
TomRunge95 <150838999+TomRunge95@users.noreply.github.com>
2025-03-21 11:26:14 +0100

    Revert "Update reset_password_ui.R"
    This reverts commit 432f74cdb3a498b6f0512b9503fe8d0ecde67a97.
    
    Conflicts:
    	R/reset_password_ui.R

--- R/reset_password_ui.R
+++ R/reset_password_ui.R
@@ -14,7 +14,11 @@ reset_password_ui <- function(id) {
 			x = div(
 				style = 'background-color: #f0f0f0; padding: 20px;border-radius: 10px;',
 				uiOutput(NS(id, 'reset_password_ui')),
+<<<<<<< HEAD
 			# value = use_login(),
+=======
+			value = use_login(),
+>>>>>>> parent of 432f74c... Update reset_password_ui.R
 			append = TRUE
 		)
 	)
