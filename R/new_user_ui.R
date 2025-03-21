bdbd810817bb349fc338e0ce603e3ebef7048977
TomRunge95 <150838999+TomRunge95@users.noreply.github.com>
2025-03-21 11:01:47 +0100

    Update new_user_ui.R and reset_password_ui.R

--- R/new_user_ui.R
+++ R/new_user_ui.R
@@ -6,19 +6,12 @@
 #' @return `shiny` object containing the input fields for a user to create an account.
 #' @export
 new_user_ui <- function(id) {
-	dependencies <- cookies::cookie_dependency()
-	dependencies[[length(dependencies) + 1]] <- use_login()
-
-	div(style = 'background-color: #f0f0f0; padding: 20px;',
-		# Dies sorgt dafür, dass es neu ausgewertet wird, wenn sich der logged_in-Status ändert
-		div(textOutput(NS(id, 'logged_in')), style = 'visibility: hidden;'),
-		conditionalPanel(
-			condition = paste0("output['", NS(id, 'logged_in'), "'] != 'TRUE'"),
-			htmltools::attachDependencies(
-				x = uiOutput(NS(id, 'new_user_ui')),
-				value = dependencies,
-				append = FALSE
-			)
+	is_not_logged_in(
+		id = id,
+		htmltools::attachDependencies(
+			x = uiOutput(NS(id, 'new_user_ui')),
+			value = use_login(),
+			append = TRUE
 		)
 	)
 }
--- R/reset_password_ui.R
+++ R/reset_password_ui.R
@@ -8,19 +8,12 @@
 #' @return a `shiny` object containing the input fields for a user to reset their password.
 #' @export
 reset_password_ui <- function(id) {
-	dependencies <- cookies::cookie_dependency()
-	dependencies[[length(dependencies) + 1]] <- use_login()
-
-	div(style = 'background-color: #f0f0f0; padding: 20px;',
-		# Dies sorgt dafür, dass es neu ausgewertet wird, wenn sich der logged_in-Status ändert
-		div(textOutput(NS(id, 'logged_in')), style = 'visibility: hidden;'),
-		conditionalPanel(
-			condition = paste0("output['", NS(id, 'logged_in'), "'] != 'TRUE'"),
-			htmltools::attachDependencies(
-				x = uiOutput(NS(id, 'reset_password_ui')),
-				value = dependencies,
-				append = FALSE
-			)
-		)
+	# is_not_logged_in(
+	# 	id = id,
+	htmltools::attachDependencies(
+		x = uiOutput(NS(id, 'reset_password_ui')),
+		value = use_login(),
+		append = TRUE
 	)
+	# )
 }
